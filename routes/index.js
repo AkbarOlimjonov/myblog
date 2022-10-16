d2de462cf9f99696cd2a39f276a0413572fde43f
Akbarshox <uanvar559@gmail.com>
2022-10-16 19:33:00 +0500

    feat: create navbar and categories

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyBlog - Akbarshox' });
+  res.render('index', { title: 'IT-blog' });
+});
+
+router.get('/all', function(req, res, next) {
+  res.render('index', { title: '' });
 });
 
 module.exports = router;
